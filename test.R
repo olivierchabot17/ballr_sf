d40f46148290b1778dd861925a3462c960622f8f
Olivier Chabot <61291290+olivierchabot17@users.noreply.github.com>
2021-09-27 12:07:48 -0400

    Add files via upload

--- test.R
+++ test.R
@@ -12,3 +12,26 @@ plot_court(court_theme = court_themes$dark)
 
 # Load shot data
 shots <- readRDS(file = "shots.rds")
+
+plot_court(court_theme = court_themes$light) +
+  geom_point(
+    data = shots %>% 
+      filter(free_throw == 0),
+    aes(x = loc_x, y = loc_y, color = shot_made_factor),
+    alpha = 0.4
+  ) +
+  theme(legend.title = element_blank())
+
+
+plot_court(court_theme = court_themes$light) +
+  geom_point(
+    data = shots %>% 
+      filter(
+        #player %in% c("Player 1", "Player 2", "Player 3", "Player 4"),
+        free_throw == 0
+      ),
+    aes(x = loc_x, y = loc_y, color = shot_made_factor),
+    alpha = 0.4
+  ) +
+  theme(legend.title = element_blank()) +
+  facet_wrap(~player)
