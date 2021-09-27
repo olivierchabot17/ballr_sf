e077817f76e28bb1df81febdad6b69f0eaab330c
Olivier Chabot <61291290+olivierchabot17@users.noreply.github.com>
2021-09-27 11:39:33 -0400

    Add files via upload
    Initial Upload

---
+++ test.R
@@ -0,0 +1,11 @@
+# Setting the working directory
+directory <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(directory)
+
+source("fiba_court_points.R")
+
+plot_court()
+
+plot_court(court_theme = court_themes$light)
+
+# ggsave("court.png", width = 8, height = 8)
